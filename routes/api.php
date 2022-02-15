0eb1cb35ab9e4c80833e083259d46d30a6be61a9
Vmbugua <vmbugua@cytonn.com>
2022-02-15 19:53:34 +0300

    moved routes from web to api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,15 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/documents', 'DocumentsController@index');
+Route::post('/documents', 'DocumentsController@store');
+Route::get('/document/{docId}', 'DocumentsController@show');
+Route::put('/documents', 'DocumentsController@update');
+Route::delete('/documents', 'DocumentsController@destroy');
+
+Route::get('/categories', 'CategoriesController@index');
+Route::post('/categories', 'CategoriesController@store');
+Route::get('/category/{docId}', 'CategoriesController@show');
+Route::put('/categories', 'CategoriesController@update');
+Route::delete('/categories', 'CategoriesController@destroy');
--- routes/web.php
+++ routes/web.php
@@ -19,14 +19,4 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/documents', 'DocumentsController@index');
-Route::post('/documents', 'DocumentsController@store');
-Route::get('/document/{docId}', 'DocumentsController@show');
-Route::put('/documents', 'DocumentsController@update');
-Route::delete('/documents', 'DocumentsController@destroy');
 
-Route::get('/categories', 'CategoriesController@index');
-Route::post('/categories', 'CategoriesController@store');
-Route::get('/category/{docId}', 'CategoriesController@show');
-Route::put('/categories', 'CategoriesController@update');
-Route::delete('/categories', 'CategoriesController@destroy');
